265f8c6a1821a92be97bca787e21134469b3b587
Marco De Bona <marcodb.debona@gmail.Com>
2019-09-02 16:27:41 +0200

    fix server start

--- src/server/index.js
+++ src/server/index.js
@@ -13,4 +13,4 @@ Concert.sync();
 
 app.get("/", () => console.log("home sweet home"));
 
-app.listen(3000, () => console.log("running on port 3000"));
+app.listen(12345, () => console.log("running on port 3000"));
