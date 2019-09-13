2702c128e435597a26391e646c72544826fda298
Marco De Bona <marcodb.debona@gmail.Com>
2019-09-13 16:12:54 +0200

    tree refacto

---
+++ src/server/actions/concert.js
@@ -0,0 +1,144 @@
+import path from "path";
+import fs from "fs";
+import {Op} from "sequelize";
+import Concert from "../models/concert";
+
+export const listConcerts = (req, res) => {
+    const {artist, event, date, hour, place, showFinished} = req.query;
+    const clause = {where: {}};
+    const formatDate = new Date(date);
+
+    artist && (clause.where.artist = artist);
+    event && (clause.where.event = event);
+    date && (clause.where.date = formatDate);
+    hour && (clause.where.hour = hour);
+    place && (clause.where.place = place);
+    if (!showFinished && !date) {
+        clause.where.date = {[Op.gt]: Date.now()};
+    }
+
+    Concert.findAll(clause)
+        .then(list => res.status(200).send(list))
+        .catch(err => res.status(400).send(err));
+};
+
+export const addConcert = (req, res) => {
+    const {artist, event, date, hour, place, link, informations} = req.body;
+    const poster = req.file;
+    const formatDate = new Date(date);
+
+    if (formatDate < Date.now()) {
+        res.status(400).json({
+            status: "Invalid date",
+            message: "The given date must be higher than the current date !",
+        });
+        return;
+    }
+
+    let targetPath = ""; // TODO: put a default image path
+
+    if (poster) {
+        const fileName = `${Date.now()}.png`;
+        targetPath = path.join(__dirname, `../../uploads/${fileName}`);
+        fs.readFile(poster.path, (err, data) => {
+            if (err) {
+                res.status(400).json({
+                    status: "Can't read image",
+                    message: "The given image can't be readed !",
+                });
+                return;
+            }
+            fs.writeFileSync(targetPath, data);
+        });
+    }
+
+    Concert.create({
+        artist,
+        event,
+        date: formatDate,
+        hour,
+        place,
+        link,
+        informations,
+        poster: targetPath,
+    })
+        .then(concert => res.status(200).send(concert))
+        .catch(err => res.status(400).send(err));
+};
+
+// FIXME: (node:477) Warning: a promise was created in a handler at src/app/bin/server/routes/concert.js:183:17 but was not returned from it, see http://goo.gl/rRqMUw
+export const updateConcert = (req, res) => {
+    const {artist, event, date, hour, place, link, informations} = req.body;
+    const {id} = req.params;
+    const poster = req.file;
+    const update = {};
+
+    if (date) {
+        const formatDate = new Date(date);
+        if (formatDate < Date.now()) {
+            res.status(400).json({
+                status: "Invalid date",
+                message:
+                    "The given date must be higher than the current date !",
+            });
+        }
+        update.date = date;
+    }
+
+    Concert.findByPk(id)
+        .then(concert => {
+            hour && (update.hour = hour);
+            artist && (update.artist = artist);
+            event && (update.event = event);
+            place && (update.place = place);
+            link && (update.link = link);
+            informations && (update.informations = informations);
+
+            if (poster) {
+                if (concert.poster === "/src/app/bin/uploads/default.png") {
+                    const fileName = `${Date.now()}.png`;
+                    const targetPath = path.join(
+                        __dirname,
+                        `../../uploads/${fileName}`,
+                    );
+                    fs.readFile(poster.path, (err, data) => {
+                        if (err) {
+                            res.status(400).json({
+                                status: "Can't read image",
+                                message: "The given image can't be readed !",
+                            });
+                            return;
+                        }
+                        fs.writeFileSync(targetPath, data);
+                        update.poster = targetPath;
+                    });
+                } else {
+                    fs.readFile(poster.path, (err, data) => {
+                        if (!err) {
+                            fs.writeFileSync(concert.poster, data);
+                        }
+                    });
+                }
+            }
+            concert
+                .update(update)
+                .then(updated => res.status(200).send(updated))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(400).send(err));
+};
+
+export const deleteConcert = (req, res) => {
+    Concert.findByPk(req.params.id)
+        .then(concert => {
+            concert
+                .destroy()
+                .then(() => res.status(200).send(concert))
+                .catch(err => res.status(400).send(err));
+        })
+        .catch(err => res.status(400).send(err));
+};
+
+export const getPoster = (req, res) => {
+    res.status(200).sendFile(req.query.poster);
+};
--- src/server/routes/concert.js
+++ src/server/routes/concert.js
@@ -1,10 +1,12 @@
 import express from "express";
 import multer from "multer";
-import {Op} from "sequelize";
-import path from "path";
-import fs from "fs";
-
-import Concert from "../models/concert";
+import {
+    listConcerts,
+    addConcert,
+    updateConcert,
+    deleteConcert,
+    getPoster,
+} from "../actions/concert";
 
 const router = express.Router();
 const multParse = multer({dest: "/uploads"});
@@ -15,24 +17,7 @@ const multParse = multer({dest: "/uploads"});
  * ACTION: List concerts
  * PARAMS: query -> filters
  */
-router.get("/", (req, res) => {
-    const {artist, event, date, hour, place, showFinished} = req.query;
-    const clause = {where: {}};
-    const formatDate = new Date(date);
-
-    artist && (clause.where.artist = artist);
-    event && (clause.where.event = event);
-    date && (clause.where.date = formatDate);
-    hour && (clause.where.hour = hour);
-    place && (clause.where.place = place);
-    if (!showFinished && !date) {
-        clause.where.date = {[Op.gt]: Date.now()};
-    }
-
-    Concert.findAll(clause)
-        .then(list => res.status(200).send(list))
-        .catch(err => res.status(400).send(err));
-});
+router.get("/", listConcerts);
 
 /*
  * URI: /api/concerts
@@ -40,49 +25,7 @@ router.get("/", (req, res) => {
  * ACTION: Add concert to database
  * PARAMS: Concert informations
  */
-router.post("/", multParse.single("poster"), (req, res) => {
-    const {artist, event, date, hour, place, link, informations} = req.body;
-    const poster = req.file;
-    const formatDate = new Date(date);
-
-    if (formatDate < Date.now()) {
-        res.status(400).json({
-            status: "Invalid date",
-            message: "The given date must be higher than the current date !",
-        });
-        return;
-    }
-
-    let targetPath = ""; // TODO: put a default image path
-
-    if (poster) {
-        const fileName = `${Date.now()}.png`;
-        targetPath = path.join(__dirname, `../../uploads/${fileName}`);
-        fs.readFile(poster.path, (err, data) => {
-            if (err) {
-                res.status(400).json({
-                    status: "Can't read image",
-                    message: "The given image can't be readed !",
-                });
-                return;
-            }
-            fs.writeFileSync(targetPath, data);
-        });
-    }
-
-    Concert.create({
-        artist,
-        event,
-        date: formatDate,
-        hour,
-        place,
-        link,
-        informations,
-        poster: targetPath,
-    })
-        .then(concert => res.status(200).send(concert))
-        .catch(err => res.status(400).send(err));
-});
+router.post("/", multParse.single("poster"), addConcert);
 
 /*
  * URI: /api/concerts/:id
@@ -91,67 +34,7 @@ router.post("/", multParse.single("poster"), (req, res) => {
  * PARAMS:  params:id -> concert id
  *          body -> concert informations
  */
-// FIXME: (node:477) Warning: a promise was created in a handler at src/app/bin/server/routes/concert.js:183:17 but was not returned from it, see http://goo.gl/rRqMUw
-router.put("/:id", multParse.single("poster"), (req, res) => {
-    const {artist, event, date, hour, place, link, informations} = req.body;
-    const {id} = req.params;
-    const poster = req.file;
-    const update = {};
-
-    if (date) {
-        const formatDate = new Date(date);
-        if (formatDate < Date.now()) {
-            res.status(400).json({
-                status: "Invalid date",
-                message:
-                    "The given date must be higher than the current date !",
-            });
-        }
-        update.date = date;
-    }
-
-    Concert.findByPk(id)
-        .then(concert => {
-            hour && (update.hour = hour);
-            artist && (update.artist = artist);
-            event && (update.event = event);
-            place && (update.place = place);
-            link && (update.link = link);
-            informations && (update.informations = informations);
-
-            if (poster) {
-                if (concert.poster === "/src/app/bin/uploads/default.png") {
-                    const fileName = `${Date.now()}.png`;
-                    const targetPath = path.join(
-                        __dirname,
-                        `../../uploads/${fileName}`,
-                    );
-                    fs.readFile(poster.path, (err, data) => {
-                        if (err) {
-                            res.status(400).json({
-                                status: "Can't read image",
-                                message: "The given image can't be readed !",
-                            });
-                            return;
-                        }
-                        fs.writeFileSync(targetPath, data);
-                        update.poster = targetPath;
-                    });
-                } else {
-                    fs.readFile(poster.path, (err, data) => {
-                        if (!err) {
-                            fs.writeFileSync(concert.poster, data);
-                        }
-                    });
-                }
-            }
-            concert
-                .update(update)
-                .then(updated => res.status(200).send(updated))
-                .catch(err => res.status(400).send(err));
-        })
-        .catch(err => res.status(400).send(err));
-});
+router.put("/:id", multParse.single("poster"), updateConcert);
 
 /*
  * URI: /api/concerts/:id
@@ -159,16 +42,7 @@ router.put("/:id", multParse.single("poster"), (req, res) => {
  * ACTION: Delete target concert
  * PARAMS: id -> concert id
  */
-router.delete("/:id", (req, res) => {
-    Concert.findByPk(req.params.id)
-        .then(concert => {
-            concert
-                .destroy()
-                .then(() => res.status(200).send(concert))
-                .catch(err => res.status(400).send(err));
-        })
-        .catch(err => res.status(400).send(err));
-});
+router.delete("/:id", deleteConcert);
 
 /*
  * URI: /api/concerts/poster/:path
@@ -176,8 +50,6 @@ router.delete("/:id", (req, res) => {
  * ACTION: Get the concert poster
  * PARAMS: query.poster -> path to the poster
  */
-router.get("/poster", (req, res) => {
-    res.status(200).sendFile(req.query.poster);
-});
+router.get("/poster", getPoster);
 
 export default router;
